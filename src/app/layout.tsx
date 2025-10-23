99f8753998cfe4fb324188ed14a627da5ed1ed48
venbarc <118934661+venbarc@users.noreply.github.com>
2025-10-23 10:34:06 +0800

    put min height in body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0a0a0a] text-gray-100`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-[#0a0a0a] text-gray-100`}
       >
-        <div className="min-h-screen bg-[#0a0a0a] text-gray-100">
+        {/* <div className=" bg-[#0a0a0a] text-gray-100"> */}
           <HideNextDevTools />
           {children}
-        </div>
+        {/* </div> */}
       </body>
     </html>
   );
